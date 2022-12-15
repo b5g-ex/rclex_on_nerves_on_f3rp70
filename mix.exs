f1068b5f1007217a2eaaebdaf989bd33e774bd33
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-12-15 22:23:54 +0900

    install rclex and `mix deps.get`

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule RclexOnNervesOnF3rp70.MixProject do
       {:shoehorn, "~> 0.9.1"},
       {:ring_logger, "~> 0.8.5"},
       {:toolshed, "~> 0.2.26"},
+      {:rclex, "~> 0.8"},
 
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.13.0", targets: @all_targets},
