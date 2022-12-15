d27135cd2d3853835ab93a9668d2252f1c7a55b4
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-12-15 22:22:41 +0900

    install nerves_system_f3rp70 and set target to `:f3rp70`

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RclexOnNervesOnF3rp70.MixProject do
 
   @app :rclex_on_nerves_on_f3rp70
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :osd32mp1, :x86_64, :grisp2]
+  @all_targets [:f3rp70]
 
   def project do
     [
@@ -44,16 +44,7 @@ defmodule RclexOnNervesOnF3rp70.MixProject do
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.19", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.19", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.19", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.19", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.19", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.19", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.14", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.10", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.19", runtime: false, targets: :x86_64},
-      {:nerves_system_grisp2, "~> 0.3", runtime: false, targets: :grisp2}
+      {:nerves_system_f3rp70, "~> 0.4", runtime: false, targets: :f3rp70}
     ]
   end
 
